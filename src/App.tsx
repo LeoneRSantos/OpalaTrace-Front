e8a3a39e7f36ad382a635fbcdd027eecb347a211
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2024-05-05 23:15:59 -0300

    Estrutura inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './index.css'
+import {
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
+
+
+
+function App() {
+
+  return (
+    <>
+      {/* <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div> */}
+
+      {/* <h1>Aqui estará o sistema de rastreabilidade de Opalas</h1> */}
+    </>
+  )
+}
+
+export default App
