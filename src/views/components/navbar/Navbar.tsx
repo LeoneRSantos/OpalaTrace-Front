36c1c3de4beed2a733fd43d5e01c37cc032a4480
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2024-05-14 23:21:47 -0300

    Refatoração na NavBar

---
+++ src/views/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+function NavBar() {
+    return (
+        <div className="topnav">
+            <a className="active" href="/">Home</a>
+            <a href="/opalas">Opalas</a>
+            <a href="/rastreamento">Rastreamento</a>
+            <a href="/agentes">Agentes</a>
+            <a href="/configuracoes">Configurações</a>
+            <a href="/login">Sair</a>
+        </div>
+    )
+}
+
+export default NavBar
