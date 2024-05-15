a5f47e7aee7e5176979adaaafb466304de0330a5
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2024-05-14 23:19:10 -0300

    Refatoração na tela de configurações

---
+++ src/views/pages/Configuracoes.tsx
@@ -0,0 +1,12 @@
+function Configuracoes() {
+    return (
+        <div>
+            <div className="margin-left: 16px;">
+                <h2>Opalas</h2>
+                <p>Esta será a tela na qual será possível adicionar e atualizar opalas</p>
+            </div>
+        </div>
+    )
+}
+
+export default Configuracoes;
