256c249260699e3b1ee4e34d8fbe265fd18df594
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2024-05-14 23:17:47 -0300

    Refatoração na Home

---
+++ src/views/pages/Home.tsx
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <div>
+            <div className="margin-left: 16px;">
+                <h2>Home</h2>
+                <p>Esta será a tela na qual será possível adicionar e atualizar opalas</p>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
