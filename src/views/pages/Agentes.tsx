271cbdc97b41c3e89367d41b5cd2e55b28d39c5f
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2024-05-25 21:53:51 -0300

    Alterações em Agentes

--- src/views/pages/Agentes.tsx
+++ src/views/pages/Agentes.tsx
@@ -2,8 +2,8 @@ function Agentes() {
     return (
         <div>
             <div className="margin-left: 16px;">
-                <h2>Opalas</h2>
-                <p>Esta será a tela na qual será possível adicionar e atualizar opalas</p>
+                <h2>Agentes</h2>
+                <p>Esta será a tela na qual será possível adicionar e atualizar agentes</p>
             </div>
         </div>
     )
