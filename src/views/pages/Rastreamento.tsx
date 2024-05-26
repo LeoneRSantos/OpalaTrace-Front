d5b1880048a6d3225935258c154e9ab179baadfd
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2024-05-25 21:54:28 -0300

    Alterações em Rastreamento

--- src/views/pages/Rastreamento.tsx
+++ src/views/pages/Rastreamento.tsx
@@ -2,8 +2,8 @@ function Rastreamento() {
     return (
         <div>
             <div className="margin-left: 16px;">
-                <h2>Opalas</h2>
-                <p>Esta será a tela na qual será possível adicionar e atualizar opalas</p>
+                <h2>Rastreamento</h2>
+                <p>Esta será a tela na qual será possível ver o rastreamento, onde a opala se encontra.</p>
             </div>
         </div>
     )
