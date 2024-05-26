a81efadb0749e7b05662c966c4770d28aae3aeeb
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2024-05-25 21:54:11 -0300

    Alterações em Configurações

--- src/views/pages/Configuracoes.tsx
+++ src/views/pages/Configuracoes.tsx
@@ -2,8 +2,8 @@ function Configuracoes() {
     return (
         <div>
             <div className="margin-left: 16px;">
-                <h2>Opalas</h2>
-                <p>Esta será a tela na qual será possível adicionar e atualizar opalas</p>
+                <h2>Configurações</h2>
+                <p>Esta será a tela na qual será possível ver as configurações</p>
             </div>
         </div>
     )
