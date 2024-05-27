ba27db1f63891ec8a1963adba9a869c962824ce4
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2024-05-26 23:57:07 -0300

    Refatoração na tela Rastreamento

--- src/views/pages/Rastreamento.tsx
+++ src/views/pages/Rastreamento.tsx
@@ -1,11 +1,15 @@
 function Rastreamento() {
     return (
-        <div>
-            <div className="margin-left: 16px;">
-                <h2>Rastreamento</h2>
-                <p>Esta será a tela na qual será possível ver o rastreamento, onde a opala se encontra.</p>
+        <main className="h-screen flex w-full">
+
+            <div className="min-h-screen flex items-start justify-center w-full bg-gray-950">
+                <div className="margin-left: 16px;">
+                    <h2>Rastreamento</h2>
+                    <p>Esta será a tela na qual será possível ver o rastreamento, onde a opala se encontra.</p>
+                </div>
             </div>
-        </div>
+
+        </main>
     )
 }
 
