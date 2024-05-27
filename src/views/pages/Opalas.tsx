bb5dcf648baf550a527e9aeb2e2fcf163aa85842
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2024-05-26 23:56:55 -0300

    Refatoração na tela Opala

--- src/views/pages/Opalas.tsx
+++ src/views/pages/Opalas.tsx
@@ -1,11 +1,18 @@
 function Opalas() {
     return (
-        <div>
-            <div className="margin-left: 16px;">
-                <h2>Opalas</h2>
-                <p>Esta será a tela na qual será possível adicionar e atualizar opalas</p>
+
+        <main className="h-screen flex w-full">
+
+            <div className="min-h-screen flex items-start justify-center w-full bg-gray-950">
+                <div>
+                    <div className="margin-left: 16px;">
+                        <h2>Opalas</h2>
+                        <p>Esta será a tela na qual será possível adicionar e atualizar opalas</p>
+                    </div>
+                </div>
             </div>
-        </div>
+
+        </main>
     )
 }
 
