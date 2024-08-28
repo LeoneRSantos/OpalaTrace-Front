055b59cc3fcf95c26f57156571d66b7747cc9a72
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-08-28 17:02:20 -0300

    Identação em Formulario.

--- src/views/pages/Formulario.tsx
+++ src/views/pages/Formulario.tsx
@@ -54,7 +54,7 @@ function Formulario() {
     }, [10000])
     
     return (
-
+        
         <section className="bg-back-color h-svh flex justify-center items-center">
             <div className="bg-form-color lg:w-6/12 px-4 pt-6 shadow-4xl rounded-3xl">
                 <div className="flex flex-col min-w-0 break-words w-full mb-6  rounded-lg border-0">
