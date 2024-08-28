f76d6fc5830c2a5c84b8003e1c2936cb0c95c5e8
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-08-28 17:05:03 -0300

    Identação em Formulario.

--- src/views/pages/Formulario.tsx
+++ src/views/pages/Formulario.tsx
@@ -107,7 +107,8 @@ function Formulario() {
                                         <SelectItem value="deb21e2e-f742-4d94-80a4-b9623885244a">Varejista</SelectItem>
 
                                     </SelectContent>
-                            </Select>
+
+                                </Select>
                             </div>
 
 
@@ -116,10 +117,10 @@ function Formulario() {
                             {/* Botão */}
                             <div className="text-center mt-6">
                                 <button onClick={handleSubmit((data) => console.log(data))} className="bg-button-color text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150" type="button">
-                                Criar usuário
-                                    
+                                    Criar usuário
+
                                 </button>
-                            
+
                             </div>
                         </form >
                     </div >
