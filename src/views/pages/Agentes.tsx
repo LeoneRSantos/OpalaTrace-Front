a42a1b8a42570222066b75d4744daa8af75ad3eb
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-09-10 15:08:53 -0300

    Criação de função que transforma o id_funcao do agente no nome da função exercida na cadeia

--- src/views/pages/Agentes.tsx
+++ src/views/pages/Agentes.tsx
@@ -39,6 +39,34 @@ function Agentes() {
         getNomes()
     }, [10000])
 
+    function definirFuncao(idFuncao: string) {
+        if (idFuncao == "f6499904-c2fd-49f1-a0a2-9bfd80a6cd65") {
+            return "Lapidador";
+        }
+        if (idFuncao == "deb21e2e-f742-4d94-80a4-b9623885244a") {
+            return "Varejista";
+
+        }
+
+        if (idFuncao == "ae9f5185-e07f-4fa5-916f-2d669356b79e"){ 
+            return "Transportador";
+        }
+
+        if (idFuncao == "0d1626ef-8dab-4f4c-9128-3dd3a57c515d"){ 
+            return "Lapidador industrial";
+        }
+
+        if (idFuncao == "820529c9-4510-4b3e-9c3b-736a682fb6eb"){ 
+            return "Lapidador artesanal";
+        }
+
+        if (idFuncao == "30cb37d4-1b38-44b8-896b-40644120144c"){ 
+            return "Cliente";
+        }
+
+        // else{return "else";}
+    }
+
     
     return (
 
