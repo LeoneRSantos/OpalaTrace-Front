632c5b9466c10b23731cee2bcf2dea3f8c103143
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-09-10 17:16:53 -0300

    Alteração de trecho ativo na navbar

--- src/views/components/navbar/Navbar.tsx
+++ src/views/components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 function NavBar() {
     return (
         <div className="topnav">
-            <a className="active" href="/">Home</a>
+            <a href="/">Home</a>
             <a href="/opalas">Opalas</a>
             <a href="/rastreamento">Rastreamento</a>
             <a href="/agentes">Agentes</a>
