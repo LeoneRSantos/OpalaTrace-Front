028a0eff65b7119d4ee0b66cb08d40d4097df0f2
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-09-17 09:40:52 -0300

    Chamada de nome em Configuracoes

--- src/views/pages/Configuracoes.tsx
+++ src/views/pages/Configuracoes.tsx
@@ -96,7 +96,7 @@ function Configuracoes() {
                                     Email
                                 </dt>
                                 <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                    fulano@example.com
+                                    {nomes.at(0)?.email}
                                 </dd>
                             </div>
                             <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
