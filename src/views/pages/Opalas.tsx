7aa3492fb92e10961e1a89deef56fdb5679a0a36
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-09-19 12:07:32 -0300

    Criação do card padrão de Opalas em Opalas

--- src/views/pages/Opalas.tsx
+++ src/views/pages/Opalas.tsx
@@ -6,13 +6,19 @@ function Opalas() {
             <NavBar />
             <main className="h-screen  w-full justify-center  bg-back-color">
 
-                <div className="min-h-screen flex items-start justify-center w-full">
-                    <div>
-                        <div className="margin-left: 16px;">
-                            <h2>Opalas</h2>
-                            <p>Esta será a tela na qual será possível adicionar e atualizar opalas</p>
-                        </div>
-                    </div>
+
+                <div className="max-w-lg  mx-4 mt-4 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Opala 1</h5>
+                    </a>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Custodiante: </p>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Função: </p>
+                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-button-color rounded-lg hover:bg-button-color focus:ring-4 focus:outline-none focus:ring-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-button-color">
+                        Transferir
+                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                    </a>
                 </div>
 
             </main>
