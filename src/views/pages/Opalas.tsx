5c216d60fa7394d0892513056159f8bffa8ac163
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-09-19 15:43:13 -0300

    Chamada de nome da opala no card  de informações desta

--- src/views/pages/Opalas.tsx
+++ src/views/pages/Opalas.tsx
@@ -36,7 +36,7 @@ function Opalas() {
 
                 <div className="max-w-lg  mx-4 mt-4 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                     <a href="#">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Opala 1</h5>
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{opalas.at(0)?.name} </h5>
                     </a>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Custodiante: </p>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Função: </p>
