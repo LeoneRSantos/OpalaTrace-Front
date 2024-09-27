8e40add7b8be304c7bf5186ceae3aa6026ca4e88
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-09-26 23:08:54 -0300

    Identação em Rastreamento

--- src/views/pages/Rastreamento.tsx
+++ src/views/pages/Rastreamento.tsx
@@ -59,34 +59,12 @@ function Rastreamento() {
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                     </svg></a>
                 </li>
-                    <li className="mb-10 ms-4">
-                        <div className="absolute w-3 h-3 bg-gray-800 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                        <time className="mb-1 text-sm font-normal leading-none text-gray-500 dark:text-gray-500">2024-08-26 T21:54:10</time>
-                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Transferência de Opala 1 de lapidador para transportador</h3>
-                        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-500">Transferência da opala Opala 1 de Lapidador 1 para Transportador 1.</p>
 
-                        <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-500 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Detalhes <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                        </svg></a>
-                        
-                    </li>
-                    <li className="ms-4">
-                        <div className="absolute w-3 h-3 bg-gray-800 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                        <time className="mb-1 text-sm font-normal leading-none text-gray-500 dark:text-gray-500">2024-08-26 T21:54:10</time>
-                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Transferência de Opala 1 de transportador para varejista</h3>
-                        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-500">Transferência de Opala 1 de Transportador 1 para Varejista 1.</p>
-
-                        <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-500 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Detalhes <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                        </svg></a> 
-
-                    </li>
             </ol>
 
             ))}
 
 
-            </main>
         </>
     )
 }
