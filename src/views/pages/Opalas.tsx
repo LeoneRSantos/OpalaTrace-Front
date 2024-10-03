3af6f542cd42d01f301397be8f68bc0ae63cc4c2
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-10-03 15:54:49 -0300

    Adição de função para a definição do nome do custodiante no card em Opalas

--- src/views/pages/Opalas.tsx
+++ src/views/pages/Opalas.tsx
@@ -72,7 +72,7 @@ function Opalas() {
                         <a href="#">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Opala {cadaOpala.pool} </h5>
                         </a>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Custodiante: </p>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Custodiante: {definirNome(cadaOpala.to)}</p>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Função: </p>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">ID: {cadaOpala.id}</p>
                         <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-button-color rounded-lg hover:bg-button-color focus:ring-4 focus:outline-none focus:ring-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-button-color">
