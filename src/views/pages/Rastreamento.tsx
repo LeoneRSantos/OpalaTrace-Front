c2c89f7b75692899faa53778671dd294bb1cf7f2
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-10-06 20:55:35 -0300

    Função definirFuncao() e formatarData() em Rastreamento

--- src/views/pages/Rastreamento.tsx
+++ src/views/pages/Rastreamento.tsx
@@ -80,8 +80,48 @@ function Rastreamento() {
                                 return "Lapidador industrial";
                             }
 
+                            if (funcaoAtual == "820529c9-4510-4b3e-9c3b-736a682fb6eb") {
+                                return "Lapidador artesanal";
+                            }
 
-    const [opalas, setOpalas] = useState<Opalas[]>([])
+                            if (funcaoAtual == "30cb37d4-1b38-44b8-896b-40644120144c") {
+                                return "Cliente";
+                            }
+                            return info.id_funcao
+                        }
+                    }
+                    return element.name;
+                }
+            }
+        }
+
+        return idFuncao;
+    }
+
+
+
+    const [opalas, setOpalas] = useState<Opalas[]>([]);
+    const [infoBD, setInfoBD] = useState<Opalas[]>([]);
+    const [nomes, setNomes] = useState<Opalas[]>([]);
+
+    // Transforma a data
+    function formatarData(dateTime: string): string {
+        // Converter a string para um objeto Date
+        const date = new Date(dateTime);
+      
+        // Extrair partes da data e hora
+        const day = date.getUTCDate().toString().padStart(2, '0');
+        const month = (date.getUTCMonth() + 1).toString().padStart(2, '0'); // Mês é 0-based
+        const year = date.getUTCFullYear();
+      
+        const hours = date.getUTCHours().toString().padStart(2, '0');
+        const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+        const seconds = date.getUTCSeconds().toString().padStart(2, '0');
+      
+        // Retornar a string formatada
+        return `${day}/${month}/${year}  |  ${hours}:${minutes}:${seconds}`;
+      }
+
 
     const getOpalas = async () => {
         try {
