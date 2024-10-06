da6d7e43f787ae7533ea244284321d6c71de3ce1
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-10-06 20:50:47 -0300

    Filtragem de opalas pela chave 'transfer' em Opalas

--- src/views/pages/Opalas.tsx
+++ src/views/pages/Opalas.tsx
@@ -38,7 +38,7 @@ function Opalas() {
 
             if (Array.isArray(opala.data)) {
                 const filteredTransfers = opala.data.filter(item => item.type === "transfer");
-                setOpalas(opala.data);
+                setOpalas(filteredTransfers);
                 console.log(filteredTransfers);
             } else {
                 console.log("A resposta não é um array.");
