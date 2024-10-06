5b5dd4d879baff07019a679f3bc9da413609af26
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-10-06 20:57:15 -0300

    Adição de formatarData() na linha do tempo de Rastreamento

--- src/views/pages/Rastreamento.tsx
+++ src/views/pages/Rastreamento.tsx
@@ -162,7 +162,7 @@ function Rastreamento() {
                 <ol className="relative border-s border-gray-800 dark:border-gray-900 my-8 mx-8 mt-4">
                     <li className="mb-10 ms-4">
                         <div className="absolute w-3 h-3 bg-gray-800 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                        <time className="mb-1 text-sm font-normal leading-none text-gray-500 dark:text-gray-500">{cadaLocal.created}</time>
+                        <time className="mb-1 text-sm font-normal leading-none text-gray-500 dark:text-gray-500">{formatarData(cadaLocal.created)}</time>
                         <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Opala {cadaLocal.pool}</h3>
                         <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-500">Transferência para {cadaLocal.to}</p>
                         <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-500 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Detalhes <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
