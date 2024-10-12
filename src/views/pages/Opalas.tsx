56795d95f774862466d40ac080a6045411cbc179
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-10-12 15:32:08 -0300

    Adição do componente modal de transferência em Opalas

--- src/views/pages/Opalas.tsx
+++ src/views/pages/Opalas.tsx
@@ -140,12 +140,7 @@ function Opalas() {
                         </a>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Custodiante: {definirNome(cadaOpala.to)}</p>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Função: {definirFuncao(cadaOpala.to)}</p>
-                        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-button-color rounded-lg hover:bg-button-color focus:ring-4 focus:outline-none focus:ring-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-button-color">
-                            Transferir
-                            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                            </svg>
-                        </a>
+                        {ModalDeTransferencia(cadaOpala.pool)}
                     </div>
 
                 )
