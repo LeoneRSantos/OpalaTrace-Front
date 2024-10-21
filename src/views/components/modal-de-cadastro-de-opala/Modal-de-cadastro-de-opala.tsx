c438f361f0e0fee4b6943a904d7dc7fdabc38feb
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-10-21 16:17:53 -0300

    Adição de handleSubmit() no formulário, em ModalDeCadastroDeOpala

--- src/views/components/modal-de-cadastro-de-opala/Modal-de-cadastro-de-opala.tsx
+++ src/views/components/modal-de-cadastro-de-opala/Modal-de-cadastro-de-opala.tsx
@@ -89,6 +89,8 @@ export function ModalDeCadastroDeOpala() {
             Insira as informações solicitadas nos campos a seguir para adicionar uma nova Opala no sistema.
           </DialogDescription>
         </DialogHeader>
+        {/* Adicionando o formulário */}
+        <form onSubmit={handleSubmit(enviarDados)}>
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="name" className="text-right">
