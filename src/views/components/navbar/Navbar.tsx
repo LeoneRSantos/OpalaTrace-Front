a69716b49aad98751540d51fa054fed29e975e7b
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-10-27 18:15:55 -0300

    Adição de active e cores na NavBar

--- src/views/components/navbar/Navbar.tsx
+++ src/views/components/navbar/Navbar.tsx
@@ -1,14 +1,18 @@
+import { useLocation } from 'react-router-dom';
+import './NavBar.css';
+
 function NavBar() {
+    const location = useLocation();
+
     return (
         <div className="topnav">
-            <a href="/">Home</a>
-            <a href="/opalas">Opalas</a>
-            <a href="/rastreamento">Rastreamento</a>
-            <a href="/agentes">Agentes</a>
-            <a href="/configuracoes">Configurações</a>
-            <a href="/login">Sair</a>
+            <a href="/opalas" className={location.pathname === '/opalas' ? 'active' : ''}>Opalas</a>
+            <a href="/rastreamento" className={location.pathname === '/rastreamento' ? 'active' : ''}>Rastreamento</a>
+            <a href="/agentes" className={location.pathname === '/agentes' ? 'active' : ''}>Agentes</a>
+            <a href="/configuracoes" className={location.pathname === '/configuracoes' ? 'active' : ''}>Configurações</a>
+            <a href="/login" className={location.pathname === '/login' ? 'active' : ''}>Sair</a>
         </div>
-    )
+    );
 }
 
-export default NavBar
+export default NavBar;
