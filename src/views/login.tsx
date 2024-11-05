8848b84ee263b052593b81a8f53a4f64fb33f839
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-11-04 21:14:25 -0300

    Identação em SignIn, Login

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -30,69 +30,73 @@ export function SignIn() {
     return <Navigate to="/Opalas" />;
   } else {
     return (
-        <>
-            <section className="bg-back-color h-svh flex justify-center items-center">
-                <div className="bg-form-color lg:w-6/12 px-4 pt-6 shadow-4xl rounded-3xl">
-                    <div className="flex flex-col min-w-0 break-words w-full mb-6  rounded-lg border-0">
-                        <div className="-t mb-0 px-6 py-6">
-                            <div className="text-center mb-3">
-                                <h2 className="text-blueGray-500 text-md font-bold">
-                                    OpalaTrace
-                                </h2>
-                            </div>
-
-                        </div>
-                        <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                            <div className="text-blueGray-400 text-center mb-3 font-bold">
-
-                            </div>
-                            <form>
-
-                                <div className="relative w-full mb-3">
-                                    <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">Email</label>
-                                    <input type="email" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150 text-form-color" placeholder="Email"></input>
-                                </div>
-
-                                <div className="relative w-full mb-3">
-                                    <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="grid-password">Senha</label>
-                                    <input type="password" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150 text-form-color" placeholder="Senha"></input>
-                                </div>
-
-                                {/* Ir pro formulário*/}
-                                <div>
-                                    <label className=" flex items-center">
-                                        <span className="ml-2 text-sm font-semibold text-blueGray-600">
-                                            Ainda não está cadastrado?
-                                            <a href="/formulario" className=" text-error-color margin-left: m-2">
-                                                Criar conta
-                                            </a>
-                                        </span>
-                                    </label>
-                                </div>
-
-                                {/* Botão */}
-                                <div className="text-center mt-6">
-                                    <button className="bg-background-color text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150 border-2 border-white" type="button">
-                                        Entrar
-                                    </button>
-
-                                </div>
-
-                            </form>
-                        </div>
-                    </div>
+      <>
+        <section className="bg-back-color h-svh flex justify-center items-center">
+          <div className="bg-form-color lg:w-6/12 px-4 pt-6 shadow-4xl rounded-3xl">
+            <div className="flex flex-col min-w-0 break-words w-full mb-6 rounded-lg border-0">
+              <div className="-t mb-0 px-6 py-6">
+                <div className="text-center mb-3">
+                  <h2 className="text-blueGray-500 text-md font-bold">
+                    OpalaTrace
+                  </h2>
                 </div>
-            </section>
-        </>
+              </div>
+              <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                <div className="text-blueGray-400 text-center mb-3 font-bold"></div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="relative w-full mb-3">
+                    <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="email">
+                      Email
+                    </label>
+                    <input
+                      {...register("email", {
+                        required: { value: true, message: "Este campo está vazio" },
+                      })}
+                      type="email"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150 text-form-color"
+                      placeholder="Email"
+                    />
+                  </div>
 
-    )
+                  <div className="relative w-full mb-3">
+                    <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="senha">
+                      Senha
+                    </label>
+                    <input
+                      {...register("senha", {
+                        required: { value: true, message: "Este campo está vazio" },
+                      })}
+                      type="password"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150 text-form-color"
+                      placeholder="Senha"
+                    />
+                  </div>
 
-}
+                  <div>
+                    <label className="flex items-center">
+                      <span className="ml-2 text-sm font-semibold text-blueGray-600">
+                        Ainda não está cadastrado?
+                        <a href="/formulario" className="text-error-color ml-2">
+                          Criar conta
+                        </a>
+                      </span>
+                    </label>
+                  </div>
 
-export function logar() {
-    var login = document.getElementById("senha")
-
-
-
-    console.log(login)
+                  <div className="text-center mt-6">
+                    <button
+                      className="bg-background-color text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150 border-2 border-white"
+                      type="submit"
+                    >
+                      Entrar
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  }
 }
