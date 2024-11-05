9dea85168a1f8b5b1665de652d6d40d92ba3ca8a
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-11-04 21:24:21 -0300

    Identação em Modal-de-transferencia

--- src/views/components/modal-de-transferencia/Modal-de-transferencia.tsx
+++ src/views/components/modal-de-transferencia/Modal-de-transferencia.tsx
@@ -41,10 +41,10 @@ export function ModalDeTransferencia({ idOpala, idOrigem, indice }: ModalDeTrans
     <Dialog>
       <DialogTrigger asChild>
         <Button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-button-color rounded-lg hover:bg-button-color focus:ring-4 focus:outline-none focus:ring-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-button-color" variant="outline">
-            Transferir
-        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                            </svg>
+          Transferir
+          <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+          </svg>
         </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[600px]">
