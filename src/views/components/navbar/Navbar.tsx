13124c9f50d82295fd316e99fe2d0e79122e0366
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-11-04 21:15:34 -0300

    Adição de imports e refatoração do <a> Sair na NavBar

--- src/views/components/navbar/Navbar.tsx
+++ src/views/components/navbar/Navbar.tsx
@@ -1,8 +1,10 @@
 import { useLocation } from 'react-router-dom';
 import './NavBar.css';
+import { useAuth } from "../../../context/Auth"
 
 function NavBar() {
     const location = useLocation();
+    const { singout } = useAuth();
 
     return (
         <div className="topnav">
@@ -10,7 +12,9 @@ function NavBar() {
             <a href="/rastreamento" className={location.pathname === '/rastreamento' ? 'active' : ''}>Rastreamento</a>
             <a href="/agentes" className={location.pathname === '/agentes' ? 'active' : ''}>Agentes</a>
             <a href="/configuracoes" className={location.pathname === '/configuracoes' ? 'active' : ''}>Configurações</a>
-            <a href="/login" className={location.pathname === '/login' ? 'active' : ''}>Sair</a>
+            <a onClick={()=>{ 
+                singout();
+            }} className='cursor-pointer'>Sair</a>
         </div>
     );
 }
