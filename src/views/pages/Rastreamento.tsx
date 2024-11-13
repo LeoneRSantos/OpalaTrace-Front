8552690761a980d7bf151bc3fb7db663500ba2bd
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-11-13 16:46:28 -0300

    Importação de definirIDpeloMint() em Rastreamento

--- src/views/pages/Rastreamento.tsx
+++ src/views/pages/Rastreamento.tsx
@@ -7,6 +7,7 @@ import { useAuth } from "@/context/Auth";
 import { definirFuncaoPeloIDEthereum } from "../../utils/DefinirFuncao";
 import { definirNomePeloIDEthereum } from "../../utils/DefinirNomePeloIDEthereum";
 import { ModalDeDetalhesDoRastreamento } from "../components/modal-de-detalhes/DetalhesDoRastreamento";
+import { definirIDPeloMint } from "@/utils/DefinirMintes";
 
 function Rastreamento() {
 
