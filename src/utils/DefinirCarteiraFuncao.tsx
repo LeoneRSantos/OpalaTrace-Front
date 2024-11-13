c5054ee4bb59276b38412c4650364913ade13ab5
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-11-13 16:45:35 -0300

    Criação de função definirCarteiraPeloEmail()

--- src/utils/DefinirCarteiraFuncao.tsx
+++ src/utils/DefinirCarteiraFuncao.tsx
@@ -11,3 +11,16 @@ export function definirCarteira(idDoUsuario: string, nomes: any, Ids: any) {
     return "ID não identificado";
 }
 
+export function definirCarteiraPeloEmail(emailDoUsuario: string, nomes: any[], Ids: any[]) {
+    for (let element of nomes) {
+        for (let i of Ids) {
+            // Verifica se `profile` e `email` existem em `i` antes de acessar
+            if (i.profile && i.profile.email && element.email === i.profile.email) {
+                if (i.profile.email === emailDoUsuario) {
+                    return i.verifiers[0]?.value || "ID não identificado"; // Retorna valor se existe, senão mensagem
+                }
+            }
+        }
+    }
+    return "Não foi possível definir o ID";
+}
